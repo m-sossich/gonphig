c8d9a7a5002b630f35ec26abbaedd158f03779ed
m-sossich <mario.sossich@nubank.com.br>
2020-05-03 23:40:13 -0300

    test: improve basic testing

--- pkg/config/config_reader_test.go
+++ pkg/config/config_reader_test.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,20 +12,23 @@ import (
 const configTestFile = "config-test"
 
 type parentConfig struct {
-	Field     string `overwrite-env:"string-env"`
-	FieldFlag string `overwrite-arg:"string-flag"`
-	Child     childConfig
+	Field string `env:"string-env"`
+	Child struct {
+		IntField int `env:"int-env"`
+		Child    struct {
+			BoolField bool `env:"bool-env"`
+		}
+	}
 }
 
-type childConfig struct {
-	IntField     int `overwrite-env:"int-env"`
-	IntFlagField int `overwrite-arg:"int-flag"`
-	Child        thirdLevelConfing
-}
-
-type thirdLevelConfing struct {
-	BoolField     bool `overwrite-env:"bool-env"`
-	BoolFlagField bool `overwrite-arg:"bool-flag"`
+type withFlagsConfig struct {
+	Field string `flag:"string-flag"`
+	Child struct {
+		IntField int `flag:"int-flag"`
+		Child    struct {
+			BoolField bool `flag:"bool-flag"`
+		}
+	}
 }
 
 func TestReadConfigFromFile(t *testing.T) {
@@ -33,24 +37,50 @@ func TestReadConfigFromFile(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, "Hello", config.Field)
-	assert.Equal(t, "", config.FieldFlag)
-
 	assert.Equal(t, 1, config.Child.IntField)
-	assert.Equal(t, 0, config.Child.IntFlagField)
-
 	assert.Equal(t, true, config.Child.Child.BoolField)
-	assert.Equal(t, false, config.Child.Child.BoolFlagField)
+}
 
+func TestReadConfigFromEnvs(t *testing.T) {
 	os.Setenv("string-env", "Bye!")
 	os.Setenv("int-env", "100")
 	os.Setenv("bool-env", "false")
 
-	var envConfigs parentConfig
-	err = ReadConfiguration(configTestFile, &envConfigs)
+	var config parentConfig
+	err := ReadConfiguration(configTestFile, &config)
 	require.NoError(t, err)
 
-	assert.Equal(t, "Bye!", envConfigs.Field)
-	assert.Equal(t, 100, envConfigs.Child.IntField)
-	assert.Equal(t, false, envConfigs.Child.Child.BoolField)
+	assert.Equal(t, "Bye!", config.Field)
+	assert.Equal(t, 100, config.Child.IntField)
+	assert.Equal(t, false, config.Child.Child.BoolField)
+}
 
+func TestReadConfigFromFlags(t *testing.T) {
+	os.Args = append(os.Args, "--string-flag=DUDE")
+	os.Args = append(os.Args, "--int-flag=10000")
+	os.Args = append(os.Args, "--bool-flag=true")
+
+	var config withFlagsConfig
+	err := ReadConfiguration(configTestFile, &config)
+	require.NoError(t, err)
+
+	assert.Equal(t, "DUDE", config.Field)
+	assert.Equal(t, 10000, config.Child.IntField)
+	assert.Equal(t, true, config.Child.Child.BoolField)
+}
+
+func TestWrongFlagTypeMessage(t *testing.T) {
+	if os.Getenv("WRONG-FLAG-VALUE") == "1" {
+		os.Args = append(os.Args, "--int-flag=notAnInt")
+		var config withFlagsConfig
+		_ = ReadConfiguration(configTestFile, &config)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "-test.run=TestWrongFlagTypeMessage")
+	cmd.Env = append(os.Environ(), "WRONG-FLAG-VALUE=1")
+	err := cmd.Run()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatal("config loading should have failed due to  the wrong flag argument was used")
 }
